8f08d2ce3ccf500dd86f03ca3914c2a66fbe584b
Eldon <Birdheldon@gmail.com>
2025-05-04 01:45:30 -0600

    n-layer networks 1.0
    Really close to getting that working, I really just need to clean it up, and then start testing in python, which i quickly added to the tail end of my work. I am trying to get this to be a library remember so it will be crucial.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,90 @@
 #include <iostream>
 #include <stdio.h>
+#include <pybind11/pybind11.h>
 
-struct connected_network {
-    int x[];
-    int w1[][];
-    int a[];
-    int w2[][];
-    int b;
+PYBIND11_MODULE(simple_module, m) {
+    m.def("simple_cpp_function", &simple_cpp_function);
+}
+
+struct layer {
+
+    layer(layer* prev, layer* n, int s, int* b, int** w) {
+
+        previous = prev;
+        next = n;
+
+        size = s;
+        bias = b;
+        weight = w;
+
+    }
+
+    int* run(int* x) {
+
+        int* y = new int[next->size];
+
+        for (int i = 0; i < next->size; i++) {
+            for (int j = 0; j < size; j++) {
+                y[i] += x[j] * weight[i][j];
+            }
+            y[i] += bias[i];
+        }
+
+        if (next == nullptr) {
+            return y;
+        }
+        else {
+            return next->run(y);
+        }
+            
+    }
+    
+    layer* previous;
+    layer* next;
+
+    int size;
+    int* bias;
+    int** weight;
+    // int* x; riding myself of this, the inputs are taken at runtime instead, they are recersibly applied through the function and carried along, use them in teh funciton call instead
 
 };
 
+struct network {
 
+    layer* start;
+    int depth;
 
-
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    network(layer *initial) {
+       start = initial; 
     }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    int* run(int* x) {
+
+        return start->run(x);
+
+    }
+
+    
+};
+
+int main(){
+
+    int* test = new int[10];
+    for (int i = 0; i < 10; i++) {
+        test[i] = 0 + i;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << test[i] << std::endl;
+    }
+
+    std::cout << test[0] << std::endl;
+    std::cout << test << std::endl;
+    std::cout << &test << std::endl;
+    std::cout << &test[0] << std::endl;
+    
 }
+
 
 
 int* neural_net_3(int* x, int size_x, int* x_bias, int** w1, int size_a, int* a_bias, int** w2, int size_r) {
@@ -58,8 +115,5 @@ int* neural_net_3(int* x, int size_x, int* x_bias, int** w1, int size_a, int* a_
     }
 
     return r;
-
-
-
 
 }
