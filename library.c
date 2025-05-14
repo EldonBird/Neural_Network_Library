67bf04842d0239fd670da7988a3ff0862eceee0f
Eldon <56803340+EldonBird@users.noreply.github.com>
2025-05-13 19:07:49 -0600

    Changed to C
    Changed the code to c so that I could really easily add it to python. Didn't like the c++ method because I'm lazy.

--- library.c
+++ library.c
@@ -1,7 +1,29 @@
 #include "library.h"
 
 #include <stdio.h>
+#include <malloc.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+
+int* run_layer(struct layer* current, int* x) {
+    if (current->next == NULL) return NULL;
+
+    int* y = (int*)malloc(sizeof(int) * current->next->size);
+    for (int i = 0; i < current->next->size; i++) {
+        y[i] = 0;
+        for (int j = 0; j < current->size; j++) {
+            y[i] += x[j] * current->weight[i][j];
+        }
+        y[i] += current->bias[i];
+    }
+
+    if (current->next->next == NULL) {
+        return y;
+    } else {
+        return run_layer(current->next, y);
+    }
+}
+
+
+int* run_network(struct network* net, int* x) {
+    return run_layer(net->start, x);
 }
--- library.h
+++ library.h
@@ -1,6 +1,21 @@
 #ifndef NEURAL_NETWORK_LIBRARY_H
 #define NEURAL_NETWORK_LIBRARY_H
 
-void hello(void);
+
+struct layer {
+    struct layer* previous;
+    struct layer* next;
+
+    int size;
+    int* bias;
+    int** weight;
+};
+
+struct network {
+    struct layer* start;
+    int depth;
+};
+
+int* run_network(struct network* net, int* x);
 
 #endif //NEURAL_NETWORK_LIBRARY_H
