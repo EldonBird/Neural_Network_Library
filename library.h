7eb48ced62646fc30e1d06520e7bd0fe983fc702
Eldon <56803340+EldonBird@users.noreply.github.com>
2025-05-13 19:12:50 -0600

    whoops back to c++
    Welp we tried.

--- library.h
+++ library.h
@@ -1,21 +1,6 @@
 #ifndef NEURAL_NETWORK_LIBRARY_H
 #define NEURAL_NETWORK_LIBRARY_H
 
-
-struct layer {
-    struct layer* previous;
-    struct layer* next;
-
-    int size;
-    int* bias;
-    int** weight;
-};
-
-struct network {
-    struct layer* start;
-    int depth;
-};
-
-int* run_network(struct network* net, int* x);
+void hello(void);
 
 #endif //NEURAL_NETWORK_LIBRARY_H
